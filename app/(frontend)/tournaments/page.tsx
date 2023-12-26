1809488a5757b8931d4c486d7c353493c4918a5a
Tarje Lavik <tarje.lavik@gmail.com>
2023-12-26 13:45:44 +0100

    add title to tournaments

--- app/(frontend)/tournaments/page.tsx
+++ app/(frontend)/tournaments/page.tsx
@@ -17,6 +17,7 @@ export default function TournamentsPage() {
         </div>
       </div>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h2 className="text-3xl font-bold tracking-tight">Turneringer</h2>
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           Hei
         </div>
